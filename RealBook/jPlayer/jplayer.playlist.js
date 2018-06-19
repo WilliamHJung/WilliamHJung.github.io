49488b2c629989296f202d8644f39a1e82937ec6
WilliamHJung <33163313+WilliamHJung@users.noreply.github.com>
2018-06-19 15:48:04 -0500

    Add files via upload

--- RealBook/jPlayer/jplayer.playlist.js
+++ RealBook/jPlayer/jplayer.playlist.js
@@ -552,19 +552,9 @@ $(window).on('load',function(){
 
 /* -----------------------------Drop down Menu----------------------------- */
 jQuery(function($){
-  $('#songImage, #playerControls, .jp-playlist').click(function(e){	// select DIV Id (#) or Class (.)
+  $('#songImage, #playerControls, .jp-playlist').click(function(e){	// select DIVs by Id (#) or Class (.) and Detect if click occured then close menu
 	$(".dl_sub_dd").hide();  		// hide DIV Class
-    e.stopPropagation(); // Prevent bubbling
+    e.stopPropagation(); // Stop the click event from bubbling to parent elements
   });
-  
-		/* Detect if click occured in other parts of page and close menu 
-  $('#playerControls').click(function(e){	// select DIV Id
-    console.log( 'playerControls div' );
-	$(".dl_sub_dd").hide();			// hide DIV Class
-  });
-    $('.jp-playlist').click(function(e){	// select DIV Class
-    console.log( 'jp-playlist' );
-	$(".dl_sub_dd").hide(); 		// hide DIV Class
-  });*/
 });
 
