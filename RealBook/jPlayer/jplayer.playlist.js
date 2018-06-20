aeb12b81cacfeb38580a1177e05a5221bac04110
WilliamHJung <33163313+WilliamHJung@users.noreply.github.com>
2018-06-20 08:22:35 -0500

    Add files via upload

--- RealBook/jPlayer/jplayer.playlist.js
+++ RealBook/jPlayer/jplayer.playlist.js
@@ -537,24 +537,24 @@ obj.innerHTML = imgTag;
 return;	
 }
 
-
+/* --------------------------------Drop down Menu-------------------------------- */
 $(window).on('load',function(){
-    $('.dl_link').click(function(){
-        var submenu = $(this).parent().children('.dl_sub_dd');
+    $('.dropDownMenu_link').click(function(){
+        var submenu = $(this).parent().children('.dropDownMenu_sub_dd');
         if (submenu.css('display') == 'none') {
-            $('.dl_sub_dd').hide(); //first hide any previously showing submenu's
-            submenu.show(); //then show the current submenu
+            $('.dropDownMenu_sub_dd').hide(); 	//first hide any previously showing submenu's
+            submenu.show(); 			//then show the current submenu
         } else {
-            submenu.hide(); //hide the current submenu again
+            submenu.hide(); 			//hide the current submenu again
         }
     });
 });
 
-/* -----------------------------Drop down Menu----------------------------- */
+// select DIVs by Id (#) or Class (.) and Detect if click occured then close menu
 jQuery(function($){
-  $('#songImage, #playerControls, .jp-playlist').click(function(e){	// select DIVs by Id (#) or Class (.) and Detect if click occured then close menu
-	$(".dl_sub_dd").hide();  		// hide DIV Class
-    e.stopPropagation(); // Stop the click event from bubbling to parent elements
+  $('#songImage, #playerControls, .jp-playlist').click(function(e){	
+	$('.dropDownMenu_sub_dd').hide(); 		// hide DIV Class
+    e.stopPropagation(); 			// Stop the click event from bubbling to parent elements
   });
 });
-
+/* --------------------------------End Drop down Menu-------------------------------- */
