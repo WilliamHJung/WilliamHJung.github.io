c519b4b91116bfbc55c343c0e2751149870282b8
WilliamHJung <33163313+WilliamHJung@users.noreply.github.com>
2018-07-04 18:45:49 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -29,14 +29,3 @@ $('.type-it').typeIt({
 })
  */
  /* End of: typeIt code  */
- 
- /* Responsive Menu: Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
- /* End of: Responsive Menu  */
